329ded34e4a7f91991d90e7672e83a90ad145015
admin <admin@admins-MacBook-Pro.local>
2026-07-05 11:56:01 +0530

    FGHJ

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -22,9 +22,9 @@ const Page404 = () => {
           <div className='row'>
             <div className='col-md-12'>
               <div className='loginInner1'>
-                <div className='log-logo m-b-20 text-center'>
+                {/* <div className='log-logo m-b-20 text-center'>
                   <img src='/imgs/logo.png' className='logo-login' />
-                </div>
+                </div> */}
                 {!timerCount &&
                   <div className='featured-box-login featured-box-secundary default text-center'>
                     <div className='error-template'>
